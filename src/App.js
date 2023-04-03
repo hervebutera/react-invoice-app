5448c162f6ab36a0cc6562f2c0e28f5244f3f7b5
--replace-all <h.buterak@gmail.com>
2023-04-03 22:25:41 +0200

    feat: add invoices page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./Components/NavBar";
+import InvoicesPage from "./Pages/InvoicesPage";
+import AppLayout from "./Layouts/AppLayout";
 
 const App = () => {
   return (
-    <div className="App">
+    <AppLayout>
       <NavBar />
-      
-    </div>
+      <InvoicesPage />
+    </AppLayout>
   );
-}
+};
 
 export default App;
