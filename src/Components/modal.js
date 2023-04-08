0957cc7dcf4b11b82917c04faa300d0a03370af9
--replace-all <h.buterak@gmail.com>
2023-04-08 22:09:40 +0200

    fix: remove children elements

--- src/Components/modal.js
+++ src/Components/modal.js
@@ -1,8 +1,6 @@
-const Modal = (props) => {
+const Modal = () => {
   return (
-    <div className="absolute top-0 left-0  w-full h-full p-0 bg-[#000000] opacity-75 ">
-      {props.children}
-    </div>
+    <div className="absolute top-0 left-0  w-full h-full p-0 bg-[#000000] opacity-75 "></div>
   );
 };
 
