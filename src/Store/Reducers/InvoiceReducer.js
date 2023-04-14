e1e0b7df27ab6e6640aaf1e855ec708fe5bc5809
--replace-all <h.buterak@gmail.com>
2023-04-14 20:27:30 +0200

    feat: add and set up the store with redux

---
+++ src/Store/Reducers/InvoiceReducer.js
@@ -0,0 +1,38 @@
+const innitialState = [
+    {
+        id: "RT340",
+        senderAddress: {
+            street: "KN 234 St",
+            postCode: "P.O Box 234",
+            city: "Kigali",
+            country: "Rwanda"
+        },
+        clientAddress: {
+            clientName: "Emmanuel Maniraguha",
+            email: "emmamaniraguha@example.com",
+            street: "SH 45 St",
+            postCode: "P.O Box 367",
+            city: "Huye",
+            country: "Rwanda",
+        },
+        invoiceSentDate: "23/04/2023",
+        paymentDeadlineDate: "23/05/2023",
+        description: "Graphic design",
+        items: [
+            {
+                itemName: "Marketing banners",
+                quantity: 3,
+                price: "100",
+                itemTotal: "300",
+            }
+        ],
+        grandTotal: "300",
+        invoiceStatus: "Paid"
+    }
+];
+
+const InvoiceReducer = (state = innitialState, action) => { 
+    return state;
+}
+
+export default InvoiceReducer;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import Invoices from "./Store/Reducers/InvoiceReducer";
+
+const store = createStore(Invoices);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
