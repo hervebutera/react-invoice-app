7a82d3374fd9e99b9a434a6e045ec9c46e349c30
--replace-all <h.buterak@gmail.com>
2023-04-17 22:26:43 +0200

    feat: add filtering by status functionality

--- src/Pages/InvoiceListPage.js
+++ src/Pages/InvoiceListPage.js
@@ -2,17 +2,39 @@ import HomePageLayout from "../Layouts/HomePageLayout";
 import HomeHeader from "../Components/HomeHeader";
 import InvoicesList from "../Components/InvoicesList";
 import NewInvoice from "../Components/NewInvoice";
+import { connect } from "react-redux";
+import { useState } from "react";
 
-const InvoiceListPage = () => {
+const InvoiceListPage = (props) => {
+  const [displayedInvoices, setDisplayedInvoices] = useState(props.invoices);
+
+  const filterInvoicesByStatus = (status) => {
+    if (status === "Filter by status" || status === "All") {
+      setDisplayedInvoices(props.invoices);
+    } else {
+      setDisplayedInvoices(() => {
+        return props.invoices.filter(
+          (invoice) => invoice.invoiceStatus === status
+        );
+      });
+    }
+  };
+
   return (
     <>
       <HomePageLayout>
-        <HomeHeader />
-        <InvoicesList />
+        <HomeHeader filterByStatus={filterInvoicesByStatus} />
+        <InvoicesList invoices={displayedInvoices} />
       </HomePageLayout>
       <NewInvoice />
     </>
   );
 };
 
-export default InvoiceListPage;
+const mapStateToProps = (state) => {
+  return {
+    invoices: state,
+  };
+};
+
+export default connect(mapStateToProps)(InvoiceListPage);
