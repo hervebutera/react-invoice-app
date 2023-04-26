a8ef0deb9ba32f904e968111172f5a81277c240c
--replace-all <h.buterak@gmail.com>
2023-04-26 20:13:29 +0200

    chore(newInvoice): implement adding invoice to the store

--- src/Store/Reducers/InvoiceReducer.js
+++ src/Store/Reducers/InvoiceReducer.js
@@ -1,70 +1,74 @@
 const innitialState = [
-    {
-        id: 1,
-        invoiceCode: "RT340",
-        senderAddress: {
-            street: "KN 234 St",
-            postCode: "P.O Box 234",
-            city: "Kigali",
-            country: "Rwanda"
-        },
-        clientAddress: {
-            clientName: "Emmanuel Maniraguha",
-            email: "emmamaniraguha@example.com",
-            street: "SH 45 St",
-            postCode: "P.O Box 367",
-            city: "Huye",
-            country: "Rwanda",
-        },
-        invoiceSentDate: "23/04/2023",
-        paymentDeadlineDate: "23/05/2023",
-        description: "Graphic design",
-        items: [
-            {
-                itemName: "Marketing banners",
-                quantity: 3,
-                price: "100",
-                itemTotal: "300",
-            }
-        ],
-        grandTotal: "300",
-        invoiceStatus: "Paid"
-    },
-    {   
-        id: 2,
-        invoiceCode: "RT340",
-        senderAddress: {
-            street: "KN 234 St",
-            postCode: "P.O Box 234",
-            city: "Kigali",
-            country: "Rwanda"
-        },
-        clientAddress: {
-            clientName: "Emmanuel Maniraguha",
-            email: "emmamaniraguha@example.com",
-            street: "SH 45 St",
-            postCode: "P.O Box 367",
-            city: "Huye",
-            country: "Rwanda",
-        },
-        invoiceSentDate: "23/04/2023",
-        paymentDeadlineDate: "23/05/2023",
-        description: "Graphic design",
-        items: [
-            {
-                itemName: "Marketing banners",
-                quantity: 3,
-                price: "100",
-                itemTotal: "300",
-            }
-        ],
-        grandTotal: "300",
-        invoiceStatus: "Pending"
-    }
+  {
+    id: 1,
+    invoiceCode: "RT340",
+    senderStreetAddress: "KN 234 St",
+    senderPostCode: "P.O Box 234",
+    senderCityAddress: "Kigali",
+    senderCountryAddress: "Rwanda",
+    clientName: "Emmanuel Maniraguha",
+    clientEmail: "emmamaniraguha@example.com",
+    clientStreetAddress: "SH 45 St",
+    clientPostCode: "P.O Box 367",
+    clientCityAddress: "Huye",
+    clientCountryAddress: "Rwanda",
+    invoiceSentDate: "23/04/2023",
+    paymentDeadlineDate: "23/05/2023",
+    description: "Graphic design",
+    items: [
+      {
+        id: 0,
+        itemName: "Marketing banners",
+        quantity: 3,
+        price: 100,
+        itemTotal: 300,
+      },
+    ],
+    grandTotal: 300,
+    invoiceStatus: "Paid",
+  },
+  {
+    id: 2,
+    invoiceCode: "RT340",
+    senderStreetAddress: "KN 234 St",
+    senderPostCode: "P.O Box 234",
+    senderCityAddress: "Kigali",
+    senderCountryAddress: "Rwanda",
+    clientName: "Emmanuel Maniraguha",
+    clientEmail: "emmamaniraguha@example.com",
+    clientStreetAddress: "SH 45 St",
+    clientPostCode: "P.O Box 367",
+    clientCityAddress: "Huye",
+    clientCountryAddress: "Rwanda",
+    invoiceSentDate: "23/04/2023",
+    paymentDeadlineDate: "23/05/2023",
+    description: "Graphic design",
+    items: [
+      {
+        itemName: "Marketing banners",
+        quantity: 3,
+        price: 100,
+        itemTotal: 300,
+      },
+    ],
+    grandTotal: 300,
+    invoiceStatus: "Pending",
+  },
 ];
 
-const InvoiceReducer = (state = innitialState, action) => { 
-    return state;
-}
+const InvoiceReducer = (state = innitialState, action) => {
+  switch (action.type) {
+    case "ADD_INVOICE":
+      state = [
+        ...state,
+        action.payload
+      ];
+      break;
+  
+    default:
+      break;
+  }
+  return state;
+};
 
 export default InvoiceReducer;
