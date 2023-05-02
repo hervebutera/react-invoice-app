4970b5b784a78978e08e4d9a1268e9480f0ee5ec
--replace-all <h.buterak@gmail.com>
2023-05-02 10:53:51 +0300

    chore(invoice details page): get realtime invoice details from store

--- src/Pages/InvoiceDetailsPage.js
+++ src/Pages/InvoiceDetailsPage.js
@@ -5,11 +5,18 @@ import InvoiceDetailsHeader from "../Components/InvoiceDetailsHeader";
 import EditInvoice from "../Components/EditInvoice";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
+import { connect } from "react-redux";
 
-const InvoiceDetailsPage = () => {
+const InvoiceDetailsPage = (props) => {
   let location = useLocation();
   let { invoice } = location.state;
   const [displayInvoiceForm, setDisplayInvoiceForm] = useState(false);
+  
+  let invoiceDetails = props.invoices.filter(
+    (invoiceInfo) => {  
+      return invoiceInfo.id === invoice.id
+    }
+  );
 
   const handleDisplayInvoiceForm = (statusBoolean) => {
     return setDisplayInvoiceForm(statusBoolean);
@@ -17,22 +24,36 @@ const InvoiceDetailsPage = () => {
   return (
     <>
       <HomePageLayout>
-        <InvoiceDetailsHeader invoiceData={invoice} displayInvoiceForm={handleDisplayInvoiceForm} />
-        <InvoiceDetailsCard invoiceData={invoice} />
+        <InvoiceDetailsHeader
+          invoiceData={invoiceDetails[0]}
+          displayInvoiceForm={handleDisplayInvoiceForm}
+        />
+        <InvoiceDetailsCard invoiceData={invoiceDetails[0]} />
       </HomePageLayout>
       {displayInvoiceForm === true ? (
-        <EditInvoice invoiceData={invoice} displayInvoiceForm={handleDisplayInvoiceForm} />
+        <EditInvoice
+          invoiceData={invoiceDetails[0]}
+          displayInvoiceForm={handleDisplayInvoiceForm}
+        />
       ) : (
         ""
       )}
-        
+
       <CRUDbuttonsCard
         styles={
           " bg-invoiceItem_light_background dark:bg-invoiceItem_dark_background w-full py-4 md:hidden"
         }
+        invoiceData={invoiceDetails[0]}
+        displayInvoiceForm={handleDisplayInvoiceForm}
       />
     </>
   );
 };
 
-export default InvoiceDetailsPage;
+const mapStateToProps = (state) => {
+  return {
+    invoices: state,
+  };
+};
+
+export default connect(mapStateToProps)(InvoiceDetailsPage);
